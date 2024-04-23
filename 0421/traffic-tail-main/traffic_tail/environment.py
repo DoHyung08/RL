64ba90545c30418e83524109fa5528eb3cf26a0f
DoHyung08 <73733984+DoHyung08@users.noreply.github.com>
2024-04-23 18:40:04 +0900

    Add files via upload

---
+++ 0421/traffic-tail-main/traffic_tail/environment.py
@@ -0,0 +1,76 @@
+import traci
+from sumo_rl import SumoEnvironment
+
+'''
+Speed Mode
+bit0: Regard safe speed
+bit1: Regard maximum acceleration
+bit2: Regard maximum deceleration
+bit3: Regard right of way at intersections (only applies to approaching foe vehicles outside the intersection)
+bit4: Brake hard to avoid passing a red light
+bit5: Disregard right of way within intersections (only applies to foe vehicles that have entered the intersection).
+Setting the bit enables the check (the according value is regarded), keeping the bit==zero disables the check.
+
+Examples:
+default (all checks on) -> [0 1 1 1 1 1] -> Speed Mode = 31
+most checks off (legacy) -> [0 0 0 0 0 0] -> Speed Mode = 0
+all checks off -> [1 0 0 0 0 0] -> Speed Mode = 32
+disable right of way check -> [1 1 0 1 1 1] -> Speed Mode = 55
+run a red light [0 0 0 1 1 1] = 7 (also requires setSpeed or slowDown)
+run a red light even if the intersection is occupied [1 0 0 1 1 1] = 39 (also requires setSpeed or slowDown)
+'''
+
+
+class TailGatingEnv(SumoEnvironment):
+    def __init__(self, tailgating=True, *args, **kwargs):
+        super(TailGatingEnv, self).__init__(*args, **kwargs)
+        self.tailgating = tailgating
+                        
+    def _apply_tailgating(self, default_mode=31):
+        for tlsID in self.sumo.trafficlight.getIDList():
+            controlledLanes = self.sumo.trafficlight.getControlledLanes(tlsID)
+            stateString = self.sumo.trafficlight.getRedYellowGreenState(tlsID)
+            
+            for idx, lane in enumerate(controlledLanes):
+                vehicles = self.sumo.lane.getLastStepVehicleIDs(lane)
+                for vehID in vehicles:
+                    if 'y' in stateString[idx]:
+                        self.sumo.vehicle.setSpeedMode(vehID, 0)
+                    else:
+                        self.sumo.vehicle.setSpeedMode(vehID, default_mode)
+            
+    def _apply_realistic_impatience_gap(self):
+        for vehID in self.sumo.vehicle.getIDList():
+            impatience = self.sumo.vehicle.getImpatience(vehID)
+            minGap = 2.5 if impatience < 1 else 0.5
+            self.sumo.vehicle.setMinGap(vehID, minGap)
+
+    def _sumo_step(self):
+        if self.tailgating:
+            self._apply_tailgating()
+            self._apply_realistic_impatience_gap()
+        self.sumo.simulationStep()
+        
+
+def create_env(tailgating=False, use_gui=False, num_seconds=7200):
+    if tailgating:
+        print("Using custom environment with tailgating behavior.")
+        net_file = "nets/network.net.xml"
+        route_file = "nets/flow_tailgating.rou.xml"
+    else:
+        print("Using default SUMO environment.")
+        net_file = "nets/network.net.xml"
+        route_file = "nets/flow_default.rou.xml"
+    return TailGatingEnv(
+        tailgating=tailgating,
+        net_file=net_file,
+        route_file=route_file,
+        single_agent=False,
+        use_gui=use_gui,
+        num_seconds=num_seconds,
+        yellow_time=3,
+        min_green=5,
+        max_green=60,
+        delta_time=5,
+        sumo_warnings=False,
+    )
---
+++ 0421/traffic-tail-overspeed/traffic_tail/trainer.py
@@ -0,0 +1,121 @@
+import os
+import pickle
+from argparse import ArgumentParser
+
+from tqdm import tqdm
+from linear_rl.true_online_sarsa import TrueOnlineSarsaLambda
+from traffic_tail.environment import create_env
+import matplotlib.pyplot as plt
+
+
+class SUMOTrainer(object):
+    """
+    Main training code.
+    Train a DQN model for each module in the environment.
+    """
+    def __init__(self, env='default', num_seconds=7200, use_gui=False, graphs=False):
+        self.result_dir = f"results/{env}"
+        self.best_reward = -float('inf')
+        self.rewards = []
+        self.graphs = False
+        if(graphs):
+            self.graphs = True
+        
+        if env == 'default':
+            tailgating = False
+        elif env == 'overspeed':
+            tailgating = True
+        else:
+            raise ValueError(f"Invalid environment {env}")
+        
+        self.env = create_env(
+            tailgating=tailgating,
+            use_gui=use_gui,
+            num_seconds=num_seconds,
+        )
+        
+        print(f"Initializing RL agents. (This may take a while)")
+        self.agents = {
+            ts_id: TrueOnlineSarsaLambda(
+                self.env.observation_spaces(ts_id),
+                self.env.action_spaces(ts_id),
+                alpha=0.000000001,
+                gamma=0.95,
+                epsilon=0.05,
+                lamb=0.1,
+                fourier_order=7,
+            )
+            for ts_id in self.env.ts_ids
+        }
+    
+    def train(self, episodes=1):
+        pbar = tqdm(range(episodes * self.env.sim_max_time))
+        for episode in range(1,episodes+1):
+            total_reward = 0
+            state = self.env.reset()
+            done = {"__all__": False}
+            
+            pbar.set_description(f"Episode {episode}/{episodes}: Total Reward --")
+            while not done["__all__"]:
+                actions = {
+                    ts_id: self.agents[ts_id].act(state[ts_id]) 
+                    for ts_id in state.keys()
+                }
+                
+                next_state, reward, done, _ = self.env.step(action=actions)
+
+                for ts_id in next_state.keys():
+                    self.agents[ts_id].learn(
+                        state=next_state[ts_id], 
+                        action=actions[ts_id], 
+                        reward=reward[ts_id], 
+                        next_state=next_state[ts_id], 
+                        done=done[ts_id]
+                    )
+                    state[ts_id] = next_state[ts_id]
+                
+                total_reward += sum(reward.values())
+                pbar.update(self.env.delta_time)
+            
+            self.save(f'{self.result_dir}/pretrained_agents_run_{episode}.pkl')
+            pbar.set_description(
+                f"Episode {episode}/{episodes}: Total Reward {total_reward:.3f}"
+            )
+            print(f"Episode {episode} : Total reward {total_reward}")
+            self.rewards.append(total_reward)
+            if total_reward > self.best_reward:
+                self.best_reward = total_reward
+                self.save(f'{self.result_dir}/best_agents.pkl')
+            
+        self.env.close()
+        if self.graphs:
+            plt.plot(self.rewards)
+            plt.grid(True)
+            plt.plot()
+        return self.agents
+    
+    
+    def save(self, path=None):
+        if path is None:
+            path = os.path.join(
+                self.result_dir, 
+                'pretrained_agents.pkl'
+            )
+        with open(path, 'wb') as f:
+            pickle.dump(self.agents, f)
+            
+    def load(self, path):
+        with open(path, 'rb') as f:
+            self.agents = pickle.load(f)
+        return self
+
+
+parser = ArgumentParser()
+parser.add_argument('--use-gui', action='store_true', default=False)
+parser.add_argument('--env', type=str, default='default')
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    trainer = SUMOTrainer(env=args.env)
+    trainer.train()
